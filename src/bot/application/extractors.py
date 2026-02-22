c8c28f7e15fbfb78fde683ac6a2cba2460da20c6
David <davidberman003@gmail.com>
2026-02-22 23:14:10 +0200

    refactor: added exceptions

--- src/bot/application/extractors.py
+++ src/bot/application/extractors.py
@@ -1,6 +1,8 @@
 # application/extractors
-
+from src.bot.application.exceptions import  KeywordNotFound, CompanyNameNotFound, NotExpectedHours
 from abc import  ABC, abstractmethod
+
+
 
 class Extractor(ABC):
 
@@ -17,21 +19,49 @@ class Extractor(ABC):
         pass
 
     @staticmethod
-    def extract_by_name(table: list, company_name: str) -> int:
+    def extract_by_name(table: list, company_name: str) -> float:
         hours = 0
+        from_index = 0
         if company_name == "MOD":
-            index = table.index('סה״כ:')
-            hours = table[index - 3] # always 3 places away from סכ״ה
-        elif company_name == "Harel":
-            index = table.index('העובד:')
-            hours = table[index - 3]
-        elif company_name == "CP":
-            index = table.index('Total(Decimal)')
-            hours = table[index + 1]
-        elif company_name == "Maccabi":
-            index = table.index('הקיזוז')
-            hours = table[index - 2]
+            try:
+                index = table.index('סה״כ:')
+            except ValueError:
+                raise KeywordNotFound
+            from_index = -3
 
+        elif company_name == "Harel":
+            try:
+                index = table.index('העובד:')
+            except ValueError:
+                raise KeywordNotFound
+            from_index = -3
+
+        elif company_name == "CP":
+            try:
+                index = table.index('Total(Decimal)')
+            except ValueError:
+                raise KeywordNotFound
+            from_index = 1
+
+        elif company_name == "Maccabi":
+            try:
+                index = table.index('הקיזוז')
+            except ValueError:
+                raise KeywordNotFound
+            from_index = - 2
+
+        else:
+            raise CompanyNameNotFound
+
+        hours = table[index + from_index]
+        print(hours)
+        try:
+            hours = float(hours)
+        except ValueError:
+            raise NotExpectedHours
+
+        if 0 > hours or hours > 250:
+            raise NotExpectedHours
 
         return hours
 
