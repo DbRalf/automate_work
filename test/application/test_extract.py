2b03b7b0f6960a17c74a21c340301464e39446f3
David <davidberman003@gmail.com>
2026-02-22 20:11:39 +0200

    test: added testing for the api (allegedly)

---
+++ test/application/test_extract.py
@@ -0,0 +1,15 @@
+
+from src.bot.application.extractors import Extractor
+from src.bot.infrastructure.pdf_extractor import PDFMmanager
+from io import  BytesIO
+
+class TestExport(Extractor):
+    def extract_pdf_table(self, file) -> str:
+        pass
+
+
+
+
+
+
+
