d3ee233bf0d3f8917ff6e0d03cee19fb95d40090
David <davidberman003@gmail.com>
2026-02-22 20:05:26 +0200

    feat: the flow of the application

---
+++ src/bot/application/pipelines.py
@@ -0,0 +1,30 @@
+"""
+this file contains all the functions that handle a complete event
+email reciving/sending, creating invoices or sending them and so on
+
+"""
+
+def hour_extractor():
+    """
+    json_var,files = get_mail_info()
+    from json_var get email
+    if not in_worker_list(email) -> wait for another mail / close program
+
+
+    mes_hours = extract_message(json_var)
+    file_hours = extract_file(files)
+
+    if mes_hours and file_hours are empty throw an exception
+    if mes_hours != file_hours print to the user the hours conflict
+
+    show the user the file of the hours
+    input("what hours are in the time sheets?/ confirm [C]") somthing across those lines
+
+    create HoursReport to check domain rules
+    if all is good
+    we save it to the db (IDK what is the db yet)
+
+    :return:
+    """
+
+    pass
