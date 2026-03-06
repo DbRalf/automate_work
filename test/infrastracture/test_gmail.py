06f215fc900d3b0de99e6176e86c3d5aad0ce71d
David <davidberman003@gmail.com>
2026-03-06 21:02:05 +0200

    feat: added prints for clearer output

--- test/infrastracture/test_gmail.py
+++ test/infrastracture/test_gmail.py
@@ -1,16 +1,21 @@
 import pytest
-
+import re
 from src.bot.application.exceptions import FailedMailExtraction
 import src.bot.infrastructure.gmail_adaptor
-from src.bot.infrastructure.gmail_adaptor import get_mail_info
+from src.bot.infrastructure.gmail_adaptor import GmailAdaptor
 
 
 def test_mail_reciving ():
-    plain_text, files = get_mail_info()
+    rec_mail = GmailAdaptor()
+    plain_text, files = rec_mail.get_mail_info()
     if not plain_text or not files:
         raise FailedMailExtraction
 
     print(plain_text)
-    print(files[0].read())
+    print(f"date is: {plain_text['Date']}")
+    print(f"header is: {plain_text['Subject']}")
+    print(plain_text['body'])
+    print(plain_text["From"])
+    # print(files[0].read())
 
 
